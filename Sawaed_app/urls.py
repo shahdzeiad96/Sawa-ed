96037d3636d42498894a928e1556b5f406b6b4e4
Mohammed Iwaina <marcelo.m.iwinah@gmail.com>
2025-04-08 01:03:23 +0300

    add more service types to the  model

--- Sawaed_app/urls.py
+++ Sawaed_app/urls.py
@@ -12,13 +12,8 @@ urlpatterns = [
     path('cart/', views.cart_view, name='cart'),
     path('userhome/',views.user_home,name='userhome'),
     path('add_service/', views.add_service, name='add_service'),
-<<<<<<< HEAD
-    path('chatbot/', views.chatbot_response, name='chatbot')  #this path is temporary for chatbot later it will be added to the index page 
-]
-=======
     path('profile/edit',views.edit_profile,name='edit-profile'),
     path('logout/',views.logout,name='logout'),
     path('add_service/addservice', views.add_service, name='add_service'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 05f4f4ee7aa762280b9e34e86ae1f2ecdc54fb42
 
