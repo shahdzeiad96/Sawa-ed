3fa68435e120698cd965cf1502055866fa62cfb6
shahdzeiad96 <shahd.zeiad@gmail.com>
2025-04-07 22:52:03 +0300

    edit some links

--- Sawaed_app/static/JS/scripts.js
+++ Sawaed_app/static/JS/scripts.js
@@ -1,25 +1,30 @@
-// Function to toggle sidebar visibility
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const overlay = document.getElementById("overlay");
-
-    // Apply sliding effect with transition
-    sidebar.classList.toggle("translate-x-0");  // Slide in
-    sidebar.classList.toggle("translate-x-full");  // Slide out
-
-    // Add background overlay effect with fade-in/out
-    overlay.classList.toggle("opacity-0");
-    overlay.classList.toggle("opacity-50");
-    overlay.classList.toggle("pointer-events-none");
-}
-
-// Function to close sidebar manually
-function closeSidebar() {
+  
+    // إذا السايدبار ظاهر
+    if (sidebar.classList.contains("translate-x-0")) {
+      closeSidebar();
+    } else {
+      openSidebar();
+    }
+  }
+  
+  function openSidebar() {
     const sidebar = document.getElementById("sidebar");
     const overlay = document.getElementById("overlay");
-
-    // Slide out sidebar and fade out the overlay
+  
+    sidebar.classList.remove("translate-x-full");
+    sidebar.classList.add("translate-x-0");
+    overlay.classList.remove("hidden");
+  }
+  
+  function closeSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("overlay");
+  
+    sidebar.classList.remove("translate-x-0");
     sidebar.classList.add("translate-x-full");
-    overlay.classList.add("opacity-0");
-    overlay.classList.add("pointer-events-none");
-}
+    overlay.classList.add("hidden");
+  }
+  
