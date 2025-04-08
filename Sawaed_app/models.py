2209c1f81298c1fdb5f5cd7bada1b686ff719c13
shahdzeiad96 <shahd.zeiad@gmail.com>
2025-04-08 10:26:07 +0300

    add messages model

--- Sawaed_app/models.py
+++ Sawaed_app/models.py
@@ -136,4 +136,11 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"{self.client.username} - {self.service.name} - {self.status}"
+    
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
