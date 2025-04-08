3183f5ca5a329e7d8a8b195a14e6603e0b5b9c5b
shahdzeiad96 <shahd.zeiad@gmail.com>
2025-04-08 15:20:33 +0300

    test branch

--- Sawaed_app/urls.py
+++ Sawaed_app/urls.py
@@ -1,23 +1,16 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
-
-    path('',views.user_home,name='userhome'),
+    path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('cart/', views.cart_view, name='cart'),
-    path('userhome/',views.user_home,name='userhome'),
+    path('userhome/', views.user_home, name='userhome'),
     path('add_service/', views.add_service, name='add_service'),
-    path('profile/edit',views.edit_profile,name='edit-profile'),
-    path('logout/',views.logout,name='logout'),
-    path('add_service/addservice', views.add_service, name='add_service'),
+    path('profile/edit', views.edit_profile, name='edit-profile'),
+    path('logout/', views.logout, name='logout'),
     path('service/<int:service_id>/<int:user_id>/', views.service_detail, name='service_detail'),
     path('send-message/<int:recipient_id>/<int:service_id>/', views.send_message, name='send_message'),
-
-
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
